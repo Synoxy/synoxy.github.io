ed292f692d2e12d2d485f0883e416a59f00e9811
Dhawal <43647437+Synoxy@users.noreply.github.com>
2020-07-09 12:42:04 +0530

    Screenshare added

--- script.js
+++ script.js
@@ -113,3 +113,17 @@ function localDescCreated(desc) {
     onError
   );
 }
+function startCapture() {
+  navigator.mediaDevices.getDisplayMedia({audio:true, video:true})
+  .then(stream1 => yourVideo.srcObject = stream1)
+  .then(stream1 => pc.addStream(stream1));
+}
+function stopCapture() {
+  navigator.mediaDevices.getUserMedia({audio: true,video: true,})
+  .then(stream => {
+  // Display your local video in #localVideo element
+    localVideo.srcObject = stream;
+  // Add your stream to be sent to the conneting peer
+    pc.addStream(stream);
+  }, onError);
+}
